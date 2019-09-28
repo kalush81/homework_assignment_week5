b5ead91024172d5c57ab2ab8ab9a46a6599432ea
kalush81 <krzysiek.kaluza@gmail.com>
2019-09-28 16:16:37 +0200

    Add get and post, get all movies, create movie

--- sequelize.rest.js
+++ sequelize.rest.js
@@ -1,19 +1,65 @@
-const express = require('express');
-const Sequelize =  require('sequelize');
-const { Route } =  require('express');
+const express = require("express");
+const Sequelize = require("sequelize");
+const { Router } = require("express");
+const bodyParser = require("body-parser");
 
-const pswd = 'secret', hostname = 6544;
+const router = new Router();
 
-const databaseUrl = `postgres://postgres:${pswd}@localhost:${hostname}/postgres`
+const app = express();
+
+const serverPort = process.env.PORT || 3001;
+
+//assign a password (string) to postgress and port (integer) you are using on your local machine
+const password = "secret",
+  port = 6544;
+
+const databaseUrl =
+  process.env.DATABASE_URL ||
+  `postgres://postgres:${password}@localhost:${port}/postgres`;
+
+app.use(bodyParser.json());
 
 const db = new Sequelize(databaseUrl);
 
-const Movie = db.define('movie', {
-    title: Sequelize.STRING,
-    yearOfRelease: Sequelize.INTEGER,
-    synopsis: Sequelize.STRING
+const Movie = db.define("movie", {
+  title: {
+    type: Sequelize.STRING,
+    unique: true
+  },
+  yearOfRelease: Sequelize.INTEGER,
+  synopsis: Sequelize.STRING
 });
 
 db.sync()
-    .then(() => console.log('database synced'))
-    .catch(console.error)
+  .then(
+    Movie.create({
+      title: "Battlestar Galactica",
+      yearOfRelease: 2004,
+      synopsis: `When an old enemy, the Cylons, 
+        resurface and obliterate the 12 colonies, 
+        the crew of the aged Galactica protect a small 
+        civilian fleet - the last of humanity - as they 
+        journey toward the fabled 13th colony, Earth.`
+    })
+  )
+  .then(() => console.log("database synced"))
+  .catch(console.error);
+
+function getMovies(request, response, next) {
+  Movie.findAll()
+    .then(movies => response.status(201).send(movies))
+    .catch(next);
+}
+router.get("/movies", getMovies);
+
+
+function createMovie(request, response, next) {
+  Movie.create(request.body)
+    .then(movie => response.status(201).send(movie))
+    .catch(next);
+}
+router.post("/movies", createMovie);
+
+
+app.use(router);
+app.listen(serverPort, () => console.log(`listening on ${serverPort}`));
