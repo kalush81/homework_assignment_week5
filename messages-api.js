77c1fbe40b8c5b5d992ab8360e53b3d7ecc17d48
kalush81 <krzysiek.kaluza@gmail.com>
2019-09-28 11:45:58 +0200

    Set POST request, Add body parser from express library

--- messages-api.js
+++ messages-api.js
@@ -1,10 +1,24 @@
-const express = require('express');
+const express = require("express");
+const bodyParser =  require('body-parser');
 
 const app = express();
 const port = 3000;
 
-app.listen(port, () => console.log(`listening on ${port}`))
+const jsonParser = bodyParser.json()
+app.use(jsonParser)
 
-app.get('/', (req, res) => {
-    res.send('test')
+app.listen(port, () => console.log(`listening on ${port}`));
+
+app.get("/", (req, res) => {
+  res.send("test");
+});
+
+app.post('/messages', (req, res) => {
+
+    if (req.body.text) {
+        console.log(req.body.text)
+        return res.status(200).json({message: 'message received loud and clear'})
+    } else {
+        return res.status(400).send('something went wrong').end()
+    }
 })
